6f65807659a11cf6504e8861014dca0ee33a292a
hishamabdalla <hishvmabdalla@gmail.com>
2025-07-31 14:23:34 +0300

    Added Swagger

--- src/Tripmate.API/Program.cs
+++ src/Tripmate.API/Program.cs
@@ -11,14 +11,19 @@ namespace Tripmate.API
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            //Swagger configuration
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
