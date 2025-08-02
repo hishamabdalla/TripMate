f0f47fbed96e8b07383a0746300f8b9ed4747b9d
hishamabdalla <hishvmabdalla@gmail.com>
2025-08-03 01:26:12 +0300

    feat :Add AuthService

---
+++ src/Tripmate.Application/Services.Abstractions/Identity/IAuthService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tripmate.Application.Services.Identity.Login.DTOs;
+using Tripmate.Application.Services.Identity.Register.DTOs;
+using Tripmate.Domain.Common.Response;
+
+namespace Tripmate.Domain.Services.Interfaces.Identity
+{
+    public interface IAuthService
+    {
+       Task<ApiResponse<TokenResponse>> LoginAsync(LoginDto loginDto);
+       Task<ApiResponse<string>> RegisterAsync(RegisterDto registerDto);
+
+    }
+}
