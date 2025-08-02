2e6805e00cbe168e8b0b5ff48d5fc3d0bc0f76a5
hishamabdalla <hishvmabdalla@gmail.com>
2025-08-03 01:18:36 +0300

    feat: Add TokenService for JWT generation

---
+++ src/Tripmate.Application/Services.Abstractions/Identity/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tripmate.Domain.Common.Response;
+using Tripmate.Domain.Entities;
+
+namespace Tripmate.Application.Services.Abstractions.Identity
+{
+    public interface ITokenService
+    {
+        Task<TokenResponse> GenerateTokenAsync(ApplicationUser user);
+    }
+}
