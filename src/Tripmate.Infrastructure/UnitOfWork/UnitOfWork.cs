9416ae22c1b8f95923363efcd3f0de159310418b
hishamabdalla <hishvmabdalla@gmail.com>
2025-08-07 04:23:01 +0300

    feat:Add UnitOfWork

---
+++ src/Tripmate.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tripmate.Domain.Entities.Base;
+using Tripmate.Domain.Interfaces;
+using Tripmate.Domain.Interfaces.Repositories.Intefaces;
+using Tripmate.Infrastructure.Data.Context;
+using Tripmate.Infrastructure.Repositories;
+
+namespace Tripmate.Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly TripmateDbContext _context;
+        private readonly Hashtable _repositories = new Hashtable();
+        public UnitOfWork(TripmateDbContext context)
+        {
+            _context = context;
+            _repositories = new Hashtable();
+        }
+
+        public IGenericRepository<TEntity, TKey> Repository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+           
+            var type = typeof(TEntity).Name;
+            if (!_repositories.ContainsKey(type))
+            {
+                var repository = new GenericRepository<TEntity, TKey>(_context);
+                _repositories.Add(type, repository);
+            }
+            return (IGenericRepository<TEntity, TKey>)_repositories[type];
+
+        }
+
+        public async Task SaveChangesAsync()
+        {
+             await _context.SaveChangesAsync();
+        }
+    }
+}
