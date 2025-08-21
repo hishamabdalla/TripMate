2a6b53fa4e2047c9eafeb3258870af2724c62287
hishamabdalla <hishvmabdalla@gmail.com>
2025-08-21 03:23:35 +0300

    add: new migrations and seeding

---
+++ src/Tripmate.Infrastructure/DbHelper/Seeding/DataSeeder/Regions/RegionsSeeder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Tripmate.Domain.Entities.Models;
+using Tripmate.Domain.Exceptions;
+
+namespace Tripmate.Infrastructure.DbHelper.Seeding.DataSeeder.Regions
+{
+    public class RegionsSeeder
+    {
+        public static List<Region> GetRegions()
+        {
+            var filePath = @"..\Tripmate.Infrastructure\DbHelper\Seeding\DataSeeder\Regions\Regions.json";
+            if (!File.Exists(filePath))
+            {
+                throw new BadRequestException($"The file {filePath} does not exist.");
+            }
+            var json = File.ReadAllText(filePath);
+            var regions = JsonSerializer.Deserialize<List<Region>>(json);
+            if (regions == null)
+            {
+                throw new BadRequestException("Failed to deserialize regions from JSON.");
+            }
+            return regions;
+        }
+    }
+}
---
+++ src/Tripmate.Infrastructure/DbHelper/Seeding/Seeder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tripmate.Infrastructure.Data.Context;
+using Tripmate.Infrastructure.DbHelper.Seeding.DataSeeder.Attractions;
+using Tripmate.Infrastructure.DbHelper.Seeding.DataSeeder.Countries;
+using Tripmate.Infrastructure.DbHelper.Seeding.DataSeeder.Regions;
+
+namespace Tripmate.Infrastructure.DbHelper.Seeding
+{
+    public class Seeder:ISeeder
+    {
+        private readonly TripmateDbContext _context;
+        public Seeder(TripmateDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            if(!_context.Countries.Any())
+            {
+                var countries =CountriesSeeder.GetCountries();
+                if (countries != null)
+                {
+                    _context.Countries.AddRange(countries);
+                   await _context.SaveChangesAsync();
+                }
+            }
+            if (!_context.Regions.Any())
+            {
+                var regions = RegionsSeeder.GetRegions();
+                if (regions != null)
+                {
+                    _context.Regions.AddRange(regions);
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            if (!_context.Attractions.Any())
+            {
+                var attractions = AttractionsSeeder.GetAttractions();
+                if (attractions != null)
+                {
+                    _context.Attractions.AddRange(attractions);
+                    await _context.SaveChangesAsync();
+                }
+
+            }
+           
+        }
+    }
+}
