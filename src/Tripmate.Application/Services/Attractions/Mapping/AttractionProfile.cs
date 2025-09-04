9f60be9b39951f56d72b9eb79234589b257f9d20
hishamabdalla <hishvmabdalla@gmail.com>
2025-09-04 14:59:17 +0300

    add AttractionValidator and fix mapping issues

--- src/Tripmate.Application/Services/Attractions/Mapping/AttractionProfile.cs
+++ src/Tripmate.Application/Services/Attractions/Mapping/AttractionProfile.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Tripmate.Application.Services.Attractions.DTOs;
 using Tripmate.Application.Services.Countries.DTOs;
 using Tripmate.Domain.Entities.Models;
+using Tripmate.Domain.Enums;
 
 namespace Tripmate.Application.Services.Attractions.Mapping
 {
@@ -21,10 +22,19 @@ namespace Tripmate.Application.Services.Attractions.Mapping
 
             CreateMap<SetAttractionDto, Attraction>()
                 .ForMember(dest => dest.ImageUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.Type,
+                    opt => opt.MapFrom(src => ParseAttractionType(src.Type)))
                 .ReverseMap();
 
+        }
 
-
+        private AttractionType ParseAttractionType(string typeString)
+        {
+            if (string.IsNullOrWhiteSpace(typeString))
+                return AttractionType.Entertainment;
+            return Enum.TryParse<AttractionType>(typeString, true, out var result) && Enum.IsDefined(typeof(AttractionType), result)
+                ? result
+                : AttractionType.Entertainment;
         }
     }
 }
