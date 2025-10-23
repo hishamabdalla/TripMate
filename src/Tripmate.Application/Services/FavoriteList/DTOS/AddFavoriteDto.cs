954a1af6f9e57ba1b2891352e756e84d0a42a050
nada2710 <nadaashraf6905708@gmail.com>
2025-10-23 12:08:14 +0300

    favoriteModeland Mapping

---
+++ src/Tripmate.Application/Services/FavoriteList/DTOS/AddFavoriteDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tripmate.Domain.Enums;
+
+namespace Tripmate.Application.Services.FavoriteList.DTOS
+{
+    public class AddFavoriteDto
+    {
+        public FavoriteType FavoriteType { get; set; }
+        public int EntityId { get; set; }
+    }
+}
---
+++ src/Tripmate.Application/Services/FavoriteList/FavoriteService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tripmate.Application.Services.Abstractions.Favorite;
+using Tripmate.Application.Services.FavoriteList.DTOS;
+using Tripmate.Domain.Common.Response;
+using Tripmate.Domain.Specification.FavoriteList;
+
+namespace Tripmate.Application.Services.FavoriteList
+{
+    public class FavoriteService : IFavoriteService
+    {
+
+        public Task<PaginationResponse<IEnumerable<FavoriteResponseDto>>> GetUserFavoritesAsync(string userId, FavoriteParameters parameters)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ApiResponse<FavoriteResponseDto>> AddFavoriteAsync(string userId, AddFavoriteDto addFavoriteDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ApiResponse<bool> DeleteFavoriteAsync(int favoriteId, string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
